5c6498fa501f2514f66a0cbd7d5132db0c594b94
pengwei <pengw.rppet.cn>
2022-08-31 15:24:40 +0800

    添加proto测试

---
+++ golangRpc/golangRpcProto/goRpc01.go
@@ -0,0 +1,37 @@
+package golangRpcProto
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"micro_product/micro_proto/pc"
+	"net"
+)
+
+type HelloServiceImpl struct{}
+
+func (p *HelloServiceImpl) Hello(ctx context.Context, args *pc.StringDto) (*pc.StringDto, error) {
+
+	reply := &pc.StringDto{Value: "hello:" + args.GetValue()}
+	return reply, nil
+
+}
+
+/**
+
+首先是通过 grpc.NewServer() 构造一个 gRPC 服务对象，
+然后通过 gRPC 插件生成的 RegisterHelloServiceServer 函数注册我们实现的 HelloServiceImpl 服务。然后通过 grpcServer.Serve(lis) 在一个监听端口上提供 gRPC 服务。
+
+*/
+func RegisterHello07() {
+	grpcServer := grpc.NewServer()
+	pc.RegisterHelloServiceServer(grpcServer, new(HelloServiceImpl))
+
+	lis, err := net.Listen("tcp", ":1234")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	grpcServer.Serve(lis)
+
+}
--- golangRpc/main.go
+++ golangRpc/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"micro_product/golangRpc/golangRpc"
+	"micro_product/golangRpc/golangRpcProto"
 	_ "net/http/pprof"
 )
 
 func main() {
 
+	// =======================golang自带rpc ============================================
 	// 注册hello的rpc方法
 	//golangRpc.RegisterHello()
 
@@ -16,11 +17,15 @@ func main() {
 	//golangRpc.RegisterHello03()
 
 	//使用jsonrpc传输
-	golangRpc.RegisterHello04()
+	//golangRpc.RegisterHello04()
 
 	//golangRpc.RegisterHello05()
 
 	// 使用http的方式调用
 	//golangRpc.RegisterHello06()
+
+	//=====================================rpc+proto=================================
+
+	golangRpcProto.RegisterHello07()
 
 }
